e5122acfd4d1a69eb7ee15c583ceddab20a49769
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-12-06 15:07:12 -0300

    async/await without fetch

--- promises.js
+++ promises.js
@@ -4,36 +4,44 @@ const posts = [
 ];
 
 function getPosts() {
-  setTimeout(() => {
-    let output = "";
-    posts.forEach((post, index) => {
-      output += `<li>${post.title}</li>`;
-    });
-    document.body.innerHTML = output;
-  }, 1000);
+    setTimeout(() => {
+        let output = "";
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);
 }
 
 function createPost(post) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      posts.push(post);
-
-      const error = true;
-
-      if (!error) {
-        resolve();
-      } else {
-        reject("Error: something went wrong");
-      }
-    }, 2000);
-  });
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+            
+            const error = false;
+            
+            if (!error) {
+                resolve();
+            } else {
+                reject("Error: something went wrong");
+            }
+        }, 2000);
+    });
 }
+
+async function init() {
+    await createPost({ title: "post 3", body: "this is post 3" });
+
+    getPosts();
+}
+
+init();
 
 /* createPost({ title: "post 3", body: "this is post 3" })
   .then(getPosts)
   .catch((err) => console.log(err)); */
 
-const promise1 = Promise.resolve("Hello World");
+/* const promise1 = Promise.resolve("Hello World");
 const promise2 = 10;
 const promise3 = new Promise((resolve, reject) =>
   setTimeout(resolve, 2000, "Goodbye")
@@ -44,4 +52,4 @@ const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(
 
 Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
   console.log(values)
-);
+); */
