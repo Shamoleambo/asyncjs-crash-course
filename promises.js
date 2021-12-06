38311f3e83ecfa3e8e9392ff4e575ee298681f4d
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-12-06 14:56:42 -0300

    Promise all async

--- promises.js
+++ promises.js
@@ -29,6 +29,14 @@ function createPost(post) {
   });
 }
 
-createPost({ title: "post 3", body: "this is post 3" })
+/* createPost({ title: "post 3", body: "this is post 3" })
   .then(getPosts)
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err)); */
+
+const promise1 = Promise.resolve("Hello World");
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) =>
+  setTimeout(resolve, 2000, "Goodbye")
+);
+
+Promise.all([promise1, promise2, promise3]).then(values => console.log(values));
