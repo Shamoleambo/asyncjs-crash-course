d92ec462c89b9d570d09fc312199330af8143d7e
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-12-06 13:56:11 -0300

    callbacks async method

--- callbacks.js
+++ callbacks.js
@@ -1,14 +1,23 @@
 const posts = [
-    {title: 'post one', body: 'this is post one'},
-    {title: 'post two', body: 'this is post two'}
+  { title: "post one", body: "this is post one" },
+  { title: "post two", body: "this is post two" },
 ];
 
 function getPosts() {
-    setTimeout(()=>{
-        let output = '';
-        posts.forEach((post, index) => {
-            output += `<li>${post.title}</li>`;
-        });
-        document.body.innerHTML = output;
-    },1000);
+  setTimeout(() => {
+    let output = "";
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
 }
+
+function createPost(post, callback) {
+  setTimeout(() => {
+    posts.push(post);
+    callback();
+  }, 2000);
+}
+
+createPost({title: 'post three', body: 'This is post three'}, getPosts);
